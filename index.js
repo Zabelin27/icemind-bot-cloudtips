fc2f7405772af7b640fcc4429236e65daed6f886
Zabelin27 <zabelinanton910@gmail.com>
2025-07-07 19:12:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-const http = require('http');
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer((req, res) => {
-  res.end('IceMind bot работает!');
+const token = '7642749455:AAGY8AWxrP0yhuc6Lprzs3j3Cp5QR1JRYRQ'; // твой токен
+const bot = new TelegramBot(token);
+const app = express();
+
+app.use(bodyParser.json());
+
+// Webhook для Telegram
+app.post(`/bot${token}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
 });
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log('Сервер запущен на порту 3000');
+// Ответ на /start
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Добро пожаловать в IceMind!');
+});
+
+// Запуск сервера
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  bot.setWebHook(`https://icemind-bot-cloudtips.onrender.com/bot${token}`);
+  console.log(`Сервер запущен на порту ${PORT}`);
 });
